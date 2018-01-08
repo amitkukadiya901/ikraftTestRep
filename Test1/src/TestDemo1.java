31a0a21d3e6c5947cc3b2a079b10a9e68ba8f969
amit <amit.kukadiya9@gmail.com>
2018-01-08 17:41:10 +0530

    First change

--- Test1/src/TestDemo1.java
+++ Test1/src/TestDemo1.java
@@ -1,23 +1,7 @@
 
 public class TestDemo1 {
 	public static void main(String args[]){
-		System.out.println("A");
-		System.out.println("B");
-		System.out.println("C");
-		System.out.println("E");
-		System.out.println("D");
-		System.out.println("Z");
-		System.out.println("Z");
-		System.out.println("Z");
-		System.out.println("Z");
-		System.out.println("B");
-		System.out.println("Z");
-		System.out.println("Z");
-		System.out.println("Z");
-		System.out.println("Z");
-		System.out.println("B");
-		System.out.println("B");
-		System.out.println("B");
+		
 		System.out.println("C");
 		System.out.println("C");
 	}
