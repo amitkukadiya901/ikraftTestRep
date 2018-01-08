dd8e4a5ed1f53ede1aac7eb75fef70ef8ef65465
amitkukadiya901 <amit.kukadiya9@gmail.com>
2018-01-08 17:48:21 +0530

    changes test commmit & push.

--- Test1/src/TestDemo1.java
+++ Test1/src/TestDemo1.java
@@ -1,24 +1,10 @@
 
 public class TestDemo1 {
 	public static void main(String args[]){
-		System.out.println("A");
-		System.out.println("B");
-		System.out.println("C");
-		System.out.println("E");
-		System.out.println("D");
-		System.out.println("Z");
-		System.out.println("Z");
-		System.out.println("Z");
-		System.out.println("Z");
-		System.out.println("B");
-		System.out.println("Z");
-		System.out.println("Z");
-		System.out.println("Z");
-		System.out.println("Z");
-		System.out.println("B");
-		System.out.println("B");
-		System.out.println("B");
+		System.out.println("Test Start");
+		
 		System.out.println("C");
 		System.out.println("C");
+		System.out.println("Test End");
 	}
 }
