3c9dfe044653db474fe7d001cfcf4adb930335a6
amitkukadiya901 <amit.kukadiya9@gmail.com>
2018-01-08 16:03:45 +0530

    manually change master.

--- Test1/src/TestDemo1.java
+++ Test1/src/TestDemo1.java
@@ -3,8 +3,11 @@ public class TestDemo1 {
 	public static void main(String args[]){
 		System.out.println("A");
 		System.out.println("B");
+		System.out.println("Test1");
 		System.out.println("C");
-		System.out.println("E");
+		System.out.println("Test2");
+		System.out.println("Test3");
+		System.out.println("Test4");
 		System.out.println("D");
 		System.out.println("Z");
 		System.out.println("Z");
