6f0d4f6ce87ae07d25627c41d193998e2e066117
amitkukadiya901 <amit.kukadiya9@gmail.com>
2018-01-09 11:20:41 +0530

    push commit test demo2

--- Test1/src/TestDemo2.java
+++ Test1/src/TestDemo2.java
@@ -1,7 +1,11 @@
 
 public class TestDemo2 {
 	public static void main(String args[]){
-		System.out.println("TestDemo123");
+		System.out.println("TestDemo1");
+		System.out.println("TestDemo2");
+		System.out.println("TestDemo3");
+		System.out.println("TestDemo4");
+		System.out.println("TestDemo5");
 	}
 
 }
