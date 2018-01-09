be0a329f25a6c6e95e05799a27783b99261c11b4
amitkukadiya901 <amit.kukadiya9@gmail.com>
2018-01-09 19:37:07 +0530

    dfddfhh

--- Test1/src/TestDemo2.java
+++ Test1/src/TestDemo2.java
@@ -2,17 +2,14 @@
 public class TestDemo2 {
 	public static void main(String args[]){
 		System.out.println("TestDemo22222222222");
-		System.out.println("TestDemo22222222222");
-		System.out.println("TestDemo22222222222");
-		System.out.println("TestDemo22222222222");
-		System.out.println("TestDemodkdk");
-		System.out.println("TestDemo33333333333");
+		System.out.println("TestDemo12");
 		System.out.println("TestDemo44444444444");
 		System.out.println("TestDemo55555555555");
 		System.out.println("TestDemo10");
 		System.out.println("TestDemo11");
 		System.out.println("TestDemo66666666666");
 		System.out.println("TestDemo12");
+		System.out.println("TestDemo10");
 		System.out.println("TestDemo18");
 		System.out.println("TestDemo7");
 		System.out.println("TestDemo7");
