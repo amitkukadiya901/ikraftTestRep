ed93e19c834539e0026a815082443da9d6bfc413
amitkukadiya901 <amit.kukadiya9@gmail.com>
2018-01-09 10:53:23 +0530

    test push commit

--- Test1/src/TestDemo1.java
+++ Test1/src/TestDemo1.java
@@ -1,10 +1,10 @@
 
 public class TestDemo1 {
 	public static void main(String args[]){
-		
 		System.out.println("C");
 		System.out.println("C");
-		System.out.println("Test End");
-		System.out.println("Test End");
+		System.out.println("Test End1");
+		System.out.println("Test End2");
+		System.out.println("Test End3");
 	}
 }
