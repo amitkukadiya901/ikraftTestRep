01fb1c2ba7558dca7223077f06ef591c0b996366
amitkukadiya901 <amit.kukadiya9@gmail.com>
2018-01-09 11:24:16 +0530

    push & commit.

--- Test1/src/TestDemo1.java
+++ Test1/src/TestDemo1.java
@@ -1,14 +1,19 @@
 
 public class TestDemo1 {
 	public static void main(String args[]){
+		System.out.println("Test Start1");
+		System.out.println("Test Start2");
+		System.out.println("Test Start3");
+		System.out.println("Test Start4");
 		System.out.println("A");
 		System.out.println("B");
-		System.out.println("C");
 		System.out.println("C");
 		System.out.println("D");
 		System.out.println("Test End1");
 		System.out.println("Test End2");
 		System.out.println("Test End3");
 		System.out.println("Test End4");
+		System.out.println("Test End5");
+		System.out.println("Test End6");
 	}
 }
