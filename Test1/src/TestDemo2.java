194cd789ca4403bf3898961c1945d514569e3da5
amitkukadiya901 <amit.kukadiya9@gmail.com>
2018-01-09 15:57:21 +0530

    commit and push demo2 1

--- Test1/src/TestDemo2.java
+++ Test1/src/TestDemo2.java
@@ -1,15 +1,15 @@
 
 public class TestDemo2 {
 	public static void main(String args[]){
-		System.out.println("TestDemo1");
-		System.out.println("TestDemo2");
-		System.out.println("TestDemo3");
-		System.out.println("TestDemo4");
-		System.out.println("TestDemo5");
-		System.out.println("TestDemo6");
 		System.out.println("TestDemo7");
 		System.out.println("TestDemo8");
 		System.out.println("TestDemo9");
+		System.out.println("TestDemo10");
+		System.out.println("TestDemo11");
+		System.out.println("TestDemo12");
+		System.out.println("TestDemo13");
+		System.out.println("TestDemo14");
+		System.out.println("TestDemo15");
 	}
 
 }
