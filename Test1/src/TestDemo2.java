7f0bbcc54564741c3a2f72cc9f18bca7476a67f5
amitkukadiya901 <amit.kukadiya9@gmail.com>
2018-01-09 18:22:36 +0530

    ddddd

--- Test1/src/TestDemo2.java
+++ Test1/src/TestDemo2.java
@@ -4,8 +4,6 @@ public class TestDemo2 {
 		System.out.println("TestDemo22222222222");
 		System.out.println("TestDemo33333333333");
 		System.out.println("TestDemo44444444444");
-		System.out.println("TestDemo8");
-		System.out.println("TestDemo9");
 		System.out.println("TestDemo55555555555");
 		System.out.println("TestDemo10");
 		System.out.println("TestDemo11");
